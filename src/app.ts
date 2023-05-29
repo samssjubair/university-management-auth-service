007b97e8371fb50ea29da9fe8f3d2cee876c1a3c
samssjubair <samssjubair@gmail.com>
2023-05-29 21:58:57 +0600

    [DONE] allsetup added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import express from 'express';
+import express, { Application } from 'express';
 import { Request, Response } from 'express';
+import cors from 'cors';
 
-const app = express();
+const app: Application = express();
+app.use(cors());
+
+// parser 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
