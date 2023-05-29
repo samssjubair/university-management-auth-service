4acea5cc3549bbf9725ca277cb1ac876d4683302
samssjubair <samssjubair@gmail.com>
2023-05-29 19:14:46 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+    }
+);
+
+export default app;
