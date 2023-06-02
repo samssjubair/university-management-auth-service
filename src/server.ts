271a4f0879cfb2b9a69f185e768b13a6aa778ea9
samssjubair <samssjubair@gmail.com>
2023-06-02 16:21:01 +0600

    [DONE] eslint vscode setup improved ok2

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import config from './config/config'
 async function bootstrap() {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/test')
-    console.log('🛢 Connected to Database')
+    // console.log('🛢 Connected to Database')
     app.listen(config.port, () => {
-      console.log('🚀 Server started on port ' + config.port)
+      // console.log('🚀 Server started on port ' + config.port)
     })
   } catch (error) {
-    console.log('❌ Failed to connect to Database')
+    // console.log('❌ Failed to connect to Database')
   }
 }
 
