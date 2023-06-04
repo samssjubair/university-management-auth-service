c1bce20953b8bbf29b5f4dfa3632d31b1ea5f439
samssjubair <samssjubair@gmail.com>
2023-06-04 12:55:31 +0600

    [DONE] error handling

--- src/app/middlewares/globalErrorHandlers.ts
+++ src/app/middlewares/globalErrorHandlers.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
-import config from '../../config';
-import { IGenericErrorMessage } from '../../interfaces/error';
-import { handleValidationError } from '../../errors/handleValidationError';
-import APIError from '../../errors/ApiError';
+import config from '../../config'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import { handleValidationError } from '../../errors/handleValidationError'
+import APIError from '../../errors/ApiError'
 
 const globalErrorHandler = (
   error,
@@ -10,40 +10,36 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-    
-  let statusCode= 500;
-  let message="Something went wrong"; 
-  let errorMessages : IGenericErrorMessage[] = [];
+  let statusCode = 500
+  let message = 'Something went wrong'
+  let errorMessages: IGenericErrorMessage[] = []
 
-  if(error?.name === 'ValidationError'){
-    const simplifiedError = handleValidationError(error);
-    statusCode= simplifiedError.statusCode;
-    message= simplifiedError.message;
-    errorMessages= simplifiedError.errorMessages;
-  }
-  else if(error instanceof APIError){
-    statusCode= error?.statusCode;
-    message= error?.message;
-    errorMessages= error?.message?
-    [
-        {
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof APIError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
             path: '',
             message: error?.message,
-        }
-    ]:
-    []
-
-  }
-  else if(error instanceof Error){
-    message= error?.message;
-    errorMessages= error?.message? 
-    [
-        {
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
             path: '',
             message: error?.message,
-        }
-    ]: 
-    []
+          },
+        ]
+      : []
   }
 
   res.status(statusCode).json({
