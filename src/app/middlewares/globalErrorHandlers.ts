a97e289cf9b7ee9a183c4fdcff9c79501b9abfc8
samssjubair <samssjubair@gmail.com>
2023-06-08 03:50:26 +0600

    [DONE] zod error handler

--- src/app/middlewares/globalErrorHandlers.ts
+++ src/app/middlewares/globalErrorHandlers.ts
@@ -6,6 +6,8 @@ import { IGenericErrorMessage } from '../../interfaces/error'
 import { handleValidationError } from '../../errors/handleValidationError'
 import APIError from '../../errors/ApiError'
 import { errorLogger } from '../../shared/logger'
+import { ZodError } from 'zod'
+import handleZodValidationError from '../../errors/handleZodValidationError'
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   // to save error only in production
@@ -22,7 +24,14 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorMessages = simplifiedError.errorMessages
-  } else if (error instanceof APIError) {
+  }else if(error instanceof ZodError){
+    const simplifiedError = handleZodValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+    console.log(errorMessages)
+  }
+  else if (error instanceof APIError) {
     statusCode = error?.statusCode
     message = error?.message
     errorMessages = error?.message
--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { UserController } from './user.controller'
 import { UserValidation } from '../../middlewares/validateRequest'
+import createUserZodSchema from './user.validation'
 const router = express.Router()
 
 router.post(
   '/create-user',
-  UserValidation.validateRequest,
+  UserValidation.validateRequest(createUserZodSchema),
   UserController.createUserController
 )
 
